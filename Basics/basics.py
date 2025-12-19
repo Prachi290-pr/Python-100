3fbf37e0fbd66107e93dba00517203c207c341bf
Prachi290-pr <prachisawant38079@gmail.com>
2025-12-19 20:05:49 +0530

    Day 1

---
+++ Basics/basics.py
@@ -0,0 +1,21 @@
+# # To check python version
+# # Terminal:
+# # python --version
+#
+# # File:
+# import sys
+# print(sys.version)
+#
+# #-------
+# # Syntax : Indentation is important, no need of brackets
+# if 5 > 2 :
+#     print("5 is greater than 2")
+
+
+#-------
+# end() - multiple lines will be printed on the same line
+print("Hello!", end=" ")
+print("I will be printed on the same line")
+
+# mixing text and numbers
+print("Hello, I am", 25, "years old.")
